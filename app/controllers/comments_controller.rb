121c583ae9c99a1194c8cb3119c34ea5866c552e
Patrick McKernin <patrick@firstdraft.com>
2020-08-12 12:24:32 -0500

    changed controllers to match summer 20202 query style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,28 @@
 class CommentsController < ApplicationController
   def index
-    comments = Comment.all.order({ :created_at => :asc })
+    list_of_comments = Comment.all.order({ :created_at => :asc })
 
-    render({ :json => comments.as_json })
+    render({ :json => list_of_comments.as_json })
   end
 
   def show
     the_id = params.fetch(:the_comment_id)
-    comment = Comment.where({ :id => the_id }).at(0)
 
-    render({ :json => comment.as_json })
+    matching_comments = Comment.where({ :id => the_id })
+    
+    the_comment = matching_comments.at(0)
+    
+    render({ :json => the_comment.as_json })
   end
 
   def create
-    comment = Comment.new
+    the_comment = Comment.new
+    the_comment.author_id = params.fetch(:query_author_id, nil)
+    the_comment.photo_id = params.fetch(:query_photo_id, nil)
+    the_comment.body = params.fetch(:query_body, nil)
 
-    comment.author_id = params.fetch(:query_author_id, nil)
-    comment.photo_id = params.fetch(:query_photo_id, nil)
-    comment.body = params.fetch(:query_body, nil)
+    the_comment.save
 
-    comment.save
-
-    redirect_to("/photos/#{comment.photo_id}")
+    redirect_to("/photos/#{the_comment.photo_id}")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,29 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order({ :username => :asc })
+    all_users = User.all
+
+    @list_of_users = all_users.order({ :username => :asc })
 
     render({ :template => "user_templates/index.html" })
   end
 
   def show
     the_username = params.fetch("the_username")
-    @user = User.where({ :username => the_username }).at(0)
+   
+    matching_users = User.where({ :username => the_username })
+
+    @the_user = matching_users.at(0)
 
     render({ :template => "user_templates/show.html.erb" })
   end
 
   def create
-    user = User.new
+    the_user = User.new
 
-    user.username = params.fetch("query_username")
+    the_user.username = params.fetch("query_username")
 
-    user.save
+    the_user.save
 
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 end
